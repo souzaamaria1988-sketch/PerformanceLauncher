f8291832f05adf9e72e75b8178cec7f697845bb1
souzaamaria1988-sketch <souzaamaria1988@gmail.com>
2026-08-28 13:13:59 -0300

    Implement MainActivity with Jetpack Compose setup

---
+++ MainActivity.kt
@@ -0,0 +1,26 @@
+package com.seunome.perflauncher
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.seunome.perflauncher.ui.theme.PerformanceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PerformanceTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    PerformanceApp()
+                }
+            }
+        }
+    }
+}
