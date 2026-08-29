9685637c20a0b153d73e686e8fa644ee84db641b
souzaamaria1988-sketch <souzaamaria1988@gmail.com>
2026-08-29 09:35:18 -0300

    Refactor MainActivity to use PerformanceTheme

--- app/src/main/java/com/seunome/perflauncher/MainActivity.kt
+++ app/src/main/java/com/seunome/perflauncher/MainActivity.kt
@@ -3,22 +3,23 @@ package com.seunome.perflauncher
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.ui.graphics.Color
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.seunome.perflauncher.ui.theme.PerformanceTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Surface(
-                modifier = Modifier.fillMaxSize(),
-                color = Color(0xFF121212)
-            ) {
-                Text("Performance Launcher - Funcionando!", color = Color.White)
+            PerformanceTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    PerformanceApp()
+                }
             }
         }
     }
